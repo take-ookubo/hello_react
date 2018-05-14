526714238592e40f4890989690a8f1f9841d4cb9
take-ookubo <take.ookubo@gmail.com>
2018-05-15 04:49:49 +0900

    `npx yarn add firebase-tools` and `npx firebase login`, and `npx firebase init`

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBaTWJxyIOnpPYG8WVzWX-qrXXK_BHO6Rk",
+    authDomain: "hello-applepie.firebaseapp.com",
+    databaseURL: "https://hello-applepie.firebaseio.com",
+    projectId: "hello-applepie",
+    storageBucket: "",
+    messagingSenderId: "395924384615"
+};
